c0c46418b969ed245666d01a2418b44a18fe44e3
João Amaral <joao.amaral.almeida99@gmail.com>
2022-05-18 15:38:03 -0300

    Adding auth file and importing in user module

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AuthService } from './auth.service';
 import { User } from './user.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-  ],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AuthService],
 })
 export class UserModule {}
