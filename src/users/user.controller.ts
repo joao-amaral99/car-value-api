0e03a75838c31dbedffe3c839f6782449931911a
João Amaral <joao.amaral.almeida99@gmail.com>
2022-05-18 20:29:03 -0300

    Create signin and signup functions and using them in the user controller

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,31 +1,39 @@
-import { 
-  Controller, 
-  Post, 
-  Get, 
-  Patch, 
-  Delete, 
-  Param, 
-  Query, 
-  Body, 
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  Body,
   NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor 
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserService } from './user.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
-    this.userService.create(email, password);
+    return this.authService.signup(email, password);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+    return this.authService.signin(email, password);
   }
 
   @Get('/:id')
